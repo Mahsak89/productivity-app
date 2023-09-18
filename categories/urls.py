14f64b44fed2dedd4d68b9bc98da4db1a36872a1
Mahsak89 <>
2023-09-18 10:08:43 +0000

    added image validation and added categorylist:GEt view

---
+++ categories/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from categories import views
+urlpatterns = [
+    path('categories/', views.CategoryList.as_view()),
+]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryList(APIView):
+    serializer_class = CategorySerializer
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(
+            categories, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
