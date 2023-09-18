614f76ec4848c6500db8cd42593b50f78c4ead5b
Mahsak89 <>
2023-09-18 10:17:38 +0000

    added the ability to create a category when the user is authenticate

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Category
@@ -7,6 +7,9 @@ from .serializers import CategorySerializer
 
 class CategoryList(APIView):
     serializer_class = CategorySerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
 
     def get(self, request):
         categories = Category.objects.all()
@@ -14,3 +17,16 @@ class CategoryList(APIView):
             categories, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
