36e70de32d875b4a6a21f38c771505948951d6d1
Mahsak89 <>
2023-09-18 10:53:58 +0000

    added update and delete ability to category

--- categories/views.py
+++ categories/views.py
@@ -52,3 +52,22 @@ class CategoryDetail(APIView):
             category, context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(
+            category, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
