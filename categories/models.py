c7aa412fa6c0991907ed16b807bb4f2ce86c2b61
Mahsak89 <>
2023-09-18 07:37:34 +0000

    added(updated my model) owner field to my category model

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
