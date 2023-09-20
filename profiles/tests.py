756bbe922c53baf906372d5e7495423134cf1645
Mahsak89 <>
2023-09-20 22:33:50 +0000

    tested profiles if it can list all profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='flip', password='pass')
+
+    def test_can_list_profiles(self):
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
