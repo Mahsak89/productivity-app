cd92ee1a03191ed9ac8363552d43cfdef72735f0
Mahsak89 <>
2023-09-21 06:23:30 +0000

    added retrieve testcasee

--- profiles/tests.py
+++ profiles/tests.py
@@ -12,3 +12,15 @@ class ProfileListViewTests(APITestCase):
         response = self.client.get('/profiles/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
+
+
+class ProfileDetailViewTests(APITestCase):
+    def test_can_retrieve_profile_using_valid_id(self):
+        user = User.objects.create_user(username='flip', password='pass')
+        response = self.client.get(f'/profiles/{user.id}/')
+        self.assertEqual(response.data['id'], user.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_profile_using_invalid_id(self):
+        response = self.client.get('/profiles/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
