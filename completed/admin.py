dce3f77e7de2e99ba668f8d0e1673c37b8f26cf6
Mahsak89 <>
2023-09-23 21:57:32 +0000

    deleted completed app

--- completed/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Complete
-
-
-admin.site.register(Complete)
--- productivity_app/urls.py
+++ productivity_app/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('categories.urls')),
     path('', include('tasks.urls')),
+
 ]
