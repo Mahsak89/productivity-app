27bd56a262abde0c7e7448d852d0ae4adc7b59e6
Mahsak89 <>
2024-02-04 14:59:37 +0000

    added tag serilizer and view

--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Tag
+from .serializers import TagSerializer
+from productivity_app.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class TagList(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    queryset = Tag.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,  # Use Django filters
+
+    ]
+    filterset_fields = [
+        'owner',
+        'owner__id',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
